b4238e0978d8615c975b71f4c515e40e058f0952
kaarel <sutnucket@mail.com>
2023-08-06 23:22:03 +0300

    filled out Key constructor of Message

--- SeeSharpIRC/src/IRCModel/Message.cs
+++ SeeSharpIRC/src/IRCModel/Message.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 
 public class Message
 {
@@ -6,14 +7,30 @@ public class Message
     {
         class Key
         {
-            string client_prefix;
-            string vendor;
-            string keyname;
-            public Key(string raw_key) {
-                if (raw_key[0] == '+') {
-                    client_prefix = "+";
+            public string ClientPrefix {get;}
+            public string Vendor {get;}
+            public string Keyname {get;}
+            public Key(string rawKey) {
+                int index = 0;
+                if (rawKey[index] == '+') {
+                    ClientPrefix = "+";
+                    index += 1;
                 }
-                
+                int slashIndex = rawKey.IndexOf('/');
+                if (slashIndex != -1) {
+                    Vendor = rawKey.Substring(index, slashIndex-index);
+                    UriHostNameType vType = Uri.CheckHostName(Vendor);
+                    if (vType != UriHostNameType.Dns) {
+                        throw new Exception("Failed key parsing: Invalid vendor hostname");
+                    }
+                    index = slashIndex+1;
+                }
+                Regex rx = new Regex(@"^[a-zA-Z0-9\-]+", RegexOptions.Compiled);
+                Match keynameMatch = rx.Match(rawKey, slashIndex);
+                if (!keynameMatch.Success) {
+                    throw new Exception("Failed key parsing: Invalid key name");
+                }
+                Keyname = keynameMatch.Value;
             }
         }
         class Value
